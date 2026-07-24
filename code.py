ae78ae7effa66d7ff8e50ab8728e030f18cfaf69
billybucket18 <joandpenny@yahoo.co.uk>
2026-07-24 07:16:58 +0100

    Update hello_world function to count requests

--- code.py
+++ code.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
+requests = '0'
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    requests = str(int(requests) + 1)
+    return 'Hello from the example server! Request number: ' + requests
 
 if __name__ == '__main__':
     app.run()
